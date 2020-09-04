2ad96345925ed572830e8b5a27dd72ee7d01ea28
zac-evans <zacisadeveloper@gmail.com>
2020-09-04 00:45:35 -0400

    finished express app

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ app.use(express.static(__dirname + '/web'));
 
 
 
-
 //Register New User
 
 app.post('/register', (req, res) => {
@@ -80,32 +79,6 @@ app.post('/register', (req, res) => {
 });
 
 
-
-
-
-// //New Topic
-// /forums/:forum/topics/:topic
-
-// //New Post
-// /forums/:forum/topics/:topic/posts/:post
-
-// //New Reply
-// /forums/:forum/topics/:topic/posts/:post/replies/:reply
-
-
-
-
-app.get('/users', (req,res) => {
-    var id = req.params.id;
-    db.query(
-        'SELECT * FROM users'
-    ).then (function(results) {
-         res.json(results) 
-    })
-})
-
-
-
 //Login User
 
 app.post('/login', (req, res) => {
@@ -132,11 +105,213 @@ app.post('/login', (req, res) => {
 })
 
 
+//Create Topic
+app.post('/forums/:forum/topics', (req,res) => {
+    let forum_id = req.params.forum_id;
+    if ( req.body.topic === '' || req.body.topic === 'undefined' ) {
+        res.send('Please enter valid topic.');
+    } else if ( req.body.username_id === '' || req.body.username_id === 'undefined' ) {
+        res.send('You must be logged in to post a new topic.');
+    } else {
+        db.query(
+            `INSERT INTO topics (topic,forum_id,username_id,date_created,is_deleted)\
+            VALUES\ 
+            ('${req.body.topic}',${forum_id},${req.body.username_id},CURRENT_TIMESTAMP, FALSE)\
+            RETURNING *`)
+        .then(function (results) {
+            res.json(results);
+        }).catch(e => {
+            res.status(400).send("An error occurred.")
+        });
+    };
+});
+
+
+//Create Post
+app.post('/forums/:forum/topics/:topic/posts', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    let body = req.body.body
+    let username_id = req.body.username_id
+    if ( body === '' || body === 'undefined' ) {
+        res.send('Please enter some text in your post.');
+    } else if ( username_id === '' || username_id === 'undefined' ) {
+        res.send('You must be logged in to post.');
+    } else {
+        db.query(
+            `INSERT INTO posts (body,likes,is_deleted,forum_id,topic_id,username_id,date_created)\
+            VALUES\ 
+            ('${body}',0,FALSE,${forum_id},${topic_id},${username_id},CURRENT_TIMESTAMP)\
+            RETURNING *`)
+        .then(function (results) {
+            res.json(results)
+        }).catch(e => {
+            res.status(400).send("An error occurred.")
+        });
+    };
+});
+
+// //Create Reply
+app.post('/forums/:forum/topics/:topic/posts/:post/replies', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    let post_id = req.params.post;
+    let reply = req.body.reply;
+    let username_id = req.body.username_id;
+    if ( reply === '' || reply === 'undefined' ) {
+        res.send('Please enter some text in your reply.');
+    } else if ( username_id === '' || username_id === 'undefined' ) {
+        res.send('You must be logged in to reply.');
+    } else {
+        db.query(
+            `INSERT INTO replies (reply,likes,is_deleted,forum_id,topic_id,post_id,username_id,date_created)\
+            VALUES\ 
+            ('${reply}',0,FALSE,${forum_id},${topic_id},${post_id},${username_id},CURRENT_TIMESTAMP)\
+            RETURNING *`)
+        .then(function (results) {
+            res.json(results)
+        }).catch(e => {
+            res.status(400).send("An error occurred.")
+        });
+    };
+});
+
+
+
+//Get All Forums
+app.get('/forums', (req,res) => {
+    db.query(
+        'SELECT * FROM forum'
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("Something unexpected happened.")
+    });
+});
+
+//Get Forum
+app.get('/forums/:forum', (req,res) => {
+    let forum_id = req.params.forum;
+    db.query(
+        `SELECT * FROM forum\
+        WHERE id = '${forum_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That forum does not exist.")
+    }); 
+})
+
+//Get All Topics on Forum
+app.get('/forums/:forum/topics', (req,res) => {
+    let forum_id = req.params.forum;
+    db.query(
+        `SELECT * FROM topics\
+        WHERE forum_id = '${forum_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That forum does not exist.")
+    });
+});
+
+//Get Topic
+app.get('/forums/:forum/topics/:topic', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    db.query(
+        `SELECT * FROM topics\
+        WHERE forum_id = '${forum_id}'\
+        AND id = '${topic_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That topic does not exist.")
+    });
+});
+
+//Get All Posts on Topic
+app.get('/forums/:forum/topics/:topic/posts', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    db.query(
+        `SELECT * FROM posts\
+        WHERE forum_id = '${forum_id}'\
+        AND topic_id = '${topic_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That topic does not exist.")
+    });
+});
+
+//Get Post
+app.get('/forums/:forum/topics/:topic/posts/:post', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    let post_id = req.params.post;
+    db.query(
+        `SELECT * FROM posts\
+        WHERE forum_id = '${forum_id}'\
+        AND topic_id = '${topic_id}'\
+        AND id = '${post_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That post does not exist.")
+    });
+});
+
+//Get All Replies
+app.get('/forums/:forum/topics/:topic/posts/:post/replies', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    let post_id = req.params.post;
+    db.query(
+        `SELECT * FROM posts\
+        WHERE forum_id = '${forum_id}'\
+        AND topic_id = '${topic_id}'\
+        AND post_id = '${post_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That post does not exist.")
+    });
+});
+
+//Get Reply
+app.get('/forums/:forum/topics/:topic/posts/:post/replies/:reply', (req,res) => {
+    let forum_id = req.params.forum;
+    let topic_id = req.params.topic;
+    let post_id = req.params.post;
+    let reply_id = req.params.post;
+    db.query(
+        `SELECT * FROM posts\
+        WHERE forum_id = '${forum_id}'\
+        AND topic_id = '${topic_id}'\
+        AND post_id = '${post_id}'\
+        AND id = '${reply_id}'`
+    ).then (function(results) {
+         res.json(results) 
+    })
+    .catch(e => {
+        res.status(404).send("That reply does not exist.")
+    });
+});
+
+
 
 
 
 
 //Start localhost 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 });
