b9b1983adbcb5ac7b377136bdffb0dc8fcf4e39f
David <dcsulli@gmail.com>
2020-09-15 20:53:01 -0400

    added contact us and about me sections

---
+++ web/UserPage/ContactUs/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+    document.addEventListener("DOMContentLoaded", function(){
+        fields.yourName = document.getElementById('yourName');
+        fields.yourEmail = document.getElementById('yourEmail');
+        fields.subject = document.getElementById('subject');
+        fields.yourMessage = document.getElementById('yourMessage');
+    })
+
+    
+
+
+})
