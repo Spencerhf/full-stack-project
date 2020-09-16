a40f58d1c89035bf4a54c299635bf6bf37c98432
David <dcsulli@gmail.com>
2020-09-15 21:25:07 -0400

    added script.js

--- web/UserPage/ContactUs/script.js
+++ web/UserPage/ContactUs/script.js
@@ -1,13 +1,27 @@
 $( document ).ready(function() {
 
-    document.addEventListener("DOMContentLoaded", function(){
-        fields.yourName = document.getElementById('yourName');
-        fields.yourEmail = document.getElementById('yourEmail');
-        fields.subject = document.getElementById('subject');
-        fields.yourMessage = document.getElementById('yourMessage');
-    })
+  $('.submit').click(function (event){
+      event.preventDefault()
+      console.log(click)
 
-    
+      var name = $('.name').val()
+      var email = $('.email').val()
+      var subject = $('.subject').val()
+      var message = $('.message').val()
+      var statusElm = $('.status')
+      statusElm.empty()
 
+    if(email.length > 5 && email.includes('@') && email.includes('.')){
+        statusElm.append('<div>Email is valid</div>')
+    } else {
+        statusElm.append('<div>Email is not valid</div>')
+    }
 
+    if(subject.length > 2) {
+        statusElm.append('<div>subject is valid</div>')
+    } else {
+        statusElm.append('<div>subject is not valid</div>')
+    }
+
+  })
 })
