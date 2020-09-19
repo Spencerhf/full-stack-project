ba6e0c54a98d01c3dae7bb875e8871b48b5e2bd5
David <dcsulli@gmail.com>
2020-09-18 21:39:29 -0400

    added clearto form on submit.

--- web/notLoggedIn/ContactUs/script.js
+++ web/notLoggedIn/ContactUs/script.js
@@ -3,8 +3,8 @@ console.log('script loaded')
 
 
 
-document.getElementById('submit').addEventListener('click', function(){
-
+document.getElementById('submit').addEventListener('click', function(event){
+    event.preventDefault();
     console.log('your button works') ;
 
     const email = document.getElementById('email').value;
@@ -18,8 +18,22 @@ document.getElementById('submit').addEventListener('click', function(){
     };
     console.log('here is the content', data);
 
+    
+
     $.post('http://localhost:3000/contactus', data, function(){
     console.log('Server Received our Data');
-    });
+    });  
+
+
+    alert('your message has been sent');
+
+    function clearForm(){
+        document.getElementById('email').value='';
+        document.getElementById('subject').value='';
+        document.getElementById('message').value='';
+
+    }
+    clearForm();
+
  });
 });
